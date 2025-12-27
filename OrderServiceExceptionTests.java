1dd735b3c66960f7101dead9f68f22af5006dd38
yuta.nagano <yuta.nagano2000@gmail.com>
2025-12-27 14:37:05 +0900

    送料計算・例外スローテスト(回答)

---
+++ OrderServiceExceptionTests.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderServiceExceptionTests {
+
+    // ServiceクラスはDBアクセス等の境界をRepositoryに依存している。今回はServiceクラスのテストなので、Repositoryをモック化する。
+    @Mock
+    OrderRepository repo;
+
+    // モック化したRepositoryをServiceに注入する
+    @InjectMocks
+    OrderService service;
+
+    @Test
+    void Orderが0件の場合例外をスローする(){
+        // Arrange
+        when(repo.findById(1)).thenReturn(Optional.of(empty));
+
+        // Act & Assert
+        assertThrows(OrderNotFoundException, () -> service.calculateTotal(1));
+    }
+    
+}
